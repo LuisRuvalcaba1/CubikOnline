19a3f2745e073e462bad175b94757dc1fa4b1265
Luis <a20100360@ceti.mx>
2024-04-07 00:13:25 -0600

    Funcion basica de enviar datos del timer con webSockets

--- client/src/pages/TimerPvP.jsx
+++ client/src/pages/TimerPvP.jsx
@@ -3,29 +3,151 @@ import "./Timer.css";
 import { useForm } from 'react-hook-form';
 import io from 'socket.io-client';
 const socket = io('http://localhost:4000');
+import { useAuth } from "../context/AuthContext";
 
 function TimerPvP() {
-  const {register, handleSubmit} = useForm();
-  const onSubmit = handleSubmit((data) => {
-    socket.emit('message', data.message);
-  });
+  const { user } = useAuth();
+  const [milisegundos, setMilisegundos] = useState(0);
+  const [segundos, setSegundos] = useState(0);
+  const [minutos, setMinutos] = useState(0);
+  const [activo, setActivo] = useState(false);
+  const [tiempoInicial, setTiempoInicial] = useState(null);
+  const [scramble, setScramble] = useState("");
+  const [tiemposGuardados, setTiemposGuardados] = useState([]);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [session, setSession] = useState(1);
 
   useEffect(() => {
-    socket.on('received', (data) => {
-      console.log(data);
-    });
+    generarNuevoScramble();
   }, []);
+  
+  useEffect(() => {
+    let interval;
+    if (activo) {
+      interval = setInterval(() => {
+        const tiempoActual = performance.now();
+        const tiempoTranscurrido = tiempoActual - tiempoInicial;
+        const tiempoMilisegundos = Math.floor(tiempoTranscurrido % 1000);
+        const tiempoSegundos = Math.floor((tiempoTranscurrido / 1000) % 60);
+        const tiempoMinutos = Math.floor(
+          (tiempoTranscurrido / (1000 * 60)) % 60
+        );
 
-  return(
-    <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" 
-        {...register("message", {required: true})}
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  )
+        setMilisegundos(tiempoMilisegundos);
+        setSegundos(tiempoSegundos);
+        setMinutos(tiempoMinutos);
+      }, 1);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [activo, tiempoInicial]);
+
+  useEffect(() => {
+    function handleKeyPress(event) {
+      if (event.keyCode === 32) {
+        setActivo((prevActivo) => !prevActivo);
+        if (!activo) {
+          setTiempoInicial(performance.now());
+        } else {
+          registrarTiempo();
+          generarNuevoScramble();
+        }
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [activo]);
+
+  function generarNuevoScramble() {
+    const movimientos = ["R", "L", "U", "D", "F", "B"];
+    const modificadores = ["", "'", "2"];
+
+    let nuevoScramble = "";
+    let ultimoMovimiento = "";
+
+    for (let i = 0; i < 20; i++) {
+      let movimientoAleatorio =
+        movimientos[Math.floor(Math.random() * movimientos.length)];
+      let modificadorAleatorio =
+        modificadores[Math.floor(Math.random() * modificadores.length)];
+
+      while (movimientoAleatorio === ultimoMovimiento) {
+        movimientoAleatorio =
+          movimientos[Math.floor(Math.random() * movimientos.length)];
+      }
+
+      nuevoScramble += movimientoAleatorio + modificadorAleatorio + " ";
+      ultimoMovimiento = movimientoAleatorio;
+    }
+
+    setScramble(nuevoScramble.trim());
+  }
+
+  function registrarTiempo() {
+    if (tiempoInicial) {
+      const tiempoFinal = performance.now();
+      const tiempoTranscurrido = tiempoFinal - tiempoInicial;
+      const tiempoMilisegundos = Math.floor(tiempoTranscurrido % 1000) - 3;
+      const tiempoSegundos = Math.floor((tiempoTranscurrido / 1000) % 60);
+      const tiempoMinutos = Math.floor((tiempoTranscurrido / (1000 * 60)) % 60);
+
+      const nuevoTiempo = {
+        tiempo: `${tiempoMinutos < 10 ? `0${tiempoMinutos}` : tiempoMinutos}:${
+          tiempoSegundos < 10 ? `0${tiempoSegundos}` : tiempoSegundos
+        }:${
+          tiempoMilisegundos < 10
+            ? `00${tiempoMilisegundos}`
+            : tiempoMilisegundos < 100
+            ? `0${tiempoMilisegundos}`
+            : tiempoMilisegundos
+        }`,
+        scramble: scramble,
+      };
+
+
+      const time = `${tiempoMinutos}:${
+        tiempoSegundos < 10 ? "0" : ""
+      }${tiempoSegundos}.${
+        tiempoMilisegundos < 10 ? "00" : tiempoMilisegundos < 100 ? "0" : ""
+      }${tiempoMilisegundos}`;
+
+      setTiemposGuardados((prevTiempos) => {
+        if (Array.isArray(prevTiempos)) {
+          return [nuevoTiempo, ...prevTiempos];
+        } else {
+          console.error("prevTiempos no es un array:", prevTiempos);
+          // Devuelve un valor predeterminado o maneja este caso según sea necesario
+          return [];
+        }
+      });
+      socket.emit('message', JSON.stringify({ usuario: user._id, tiempo: time }));
+      console.log(user._id , time);
+    }
+  }
+
+  return (
+      <div className="timer__scramble">
+        <p>{scramble}</p>
+      
+      <div className="cronometro">
+        <p>
+          {minutos < 10 ? `0${minutos}` : minutos} :{" "}
+          {segundos < 10 ? `0${segundos}` : segundos} :{" "}
+          {milisegundos < 10
+            ? `00${milisegundos}`
+            : milisegundos < 100
+            ? `0${milisegundos}`
+            : milisegundos}
+        </p>
+      </div>
+      </div>
+  );
 }
 
 export default TimerPvP;
--- src/index.js
+++ src/index.js
@@ -13,14 +13,44 @@ const io = new WebSocketServer(httpServer, {
         origin: 'http://localhost:5173'
     }
 });
+
+let waitingClients = []; // Lista de clientes esperando ser emparejados
+let matchedPairs = []; // Lista de pares emparejados
+
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
 
+    // Agregar nuevo cliente a la lista de clientes esperando
+    waitingClients.push(socket);
+
+    // Emparejar a los usuarios si hay al menos dos esperando
+    if (waitingClients.length >= 2) {
+        const [user1, user2] = waitingClients.splice(0, 2); // Tomar los primeros dos clientes
+        const pair = { user1, user2 };
+        matchedPairs.push(pair);
+        user1.emit('paired', user2.id);
+        user2.emit('paired', user1.id);
+    }
+
+    // Manejar el evento de desconexión
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+        // Eliminar al cliente desconectado de la lista de clientes esperando
+        waitingClients = waitingClients.filter(client => client.id !== socket.id);
+        // Si el cliente desconectado estaba emparejado, eliminar el par emparejado
+        matchedPairs = matchedPairs.filter(pair => pair.user1.id !== socket.id && pair.user2.id !== socket.id);
+    });
+
+    // Manejar el evento de mensaje
     socket.on('message', (data) => {
-        socket.broadcast.emit("received", data)
+        // Reenviar el mensaje al otro usuario emparejado
+        const pair = matchedPairs.find(pair => pair.user1.id === socket.id || pair.user2.id === socket.id);
+        if (pair) {
+            const receiver = pair.user1.id === socket.id ? pair.user2 : pair.user1;
+            receiver.emit('message', data);
+        }
     });
 });
-
 
 console.log('Server on port 4000');
 
