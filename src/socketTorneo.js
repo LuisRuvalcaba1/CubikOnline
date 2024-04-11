7d46e213d9832f90eb06b51d599846b8bc365b74
Luis <a20100360@ceti.mx>
2024-04-11 12:52:11 -0600

    Configuracion de la pagina para crear torneos

---
+++ src/socketTorneo.js
@@ -0,0 +1,27 @@
+import {Server as WebSocketServer} from 'socket.io';
+
+export function tournamentSocket(httpServer){
+
+    const io = new WebSocketServer(httpServer, {
+        cors: {
+            origin: 'http://localhost:5173'
+        }
+    });
+    
+    let waitingClients = []; // Lista de clientes esperando ser emparejados
+    let matchedPairs = []; // Lista de pares emparejados
+    
+    io.on('connection', (socket) => {
+        socket.on('user', (user) => {
+            socket.userId = user;
+            socket.emit('user', user);
+        })
+        
+        // Agregar nuevo cliente a la lista de clientes esperando
+        waitingClients.push(socket);
+        console.log(`User connected: ${socket.userId}`);
+
+        
+
+    })
+}
