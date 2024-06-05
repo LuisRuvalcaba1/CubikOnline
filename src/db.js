d40c004baf81043e43b77bf8bbb9016e144c2db4
Luis <a20100360@ceti.mx>
2024-06-04 21:05:18 -0600

    Refactor database connection code for improved error handling and configuration

--- src/db.js
+++ src/db.js
@@ -1,12 +1,14 @@
-import mongoose from "mongoose";
-import {DB} from "./config.js";
+import mongoose from 'mongoose';
+import { DB } from './config.js';
 
 export const connectDB = async () => {
-    try{
-        await mongoose.connect(DB);
-        console.log('>>> DB is connected')
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+  try {
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('>>> DB is connected');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
+};
