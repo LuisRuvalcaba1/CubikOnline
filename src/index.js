35aab6ca2b390cb913f2fd1a325b1024ea987440
Luis <a20100360@ceti.mx>
2024-06-04 20:13:14 -0600

    Intento deploy2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import app from './app.js';
 import { connectDB } from './db.js';
 import {initializeWebSocket as socket} from './sockets.js';
 const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || '0.0.0.0';
+//const HOST = process.env.HOST || '0.0.0.0';
 connectDB();
 
 const server = http.createServer(app);
-const httpServer = server.listen(PORT, HOST);
+const httpServer = server.listen(PORT );
 socket(httpServer)
 
 
-console.log('Server on port', PORT, 'Host', HOST);
+console.log('Server on port', PORT);
